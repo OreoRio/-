b73b1cc70d4c7c946d1c7618bc30ee3074eedf1a
OreoRio <86777052+OreoRio@users.noreply.github.com>
2021-11-02 22:59:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Em yêu  à!',
+    introTitle: 'babe à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: ':< thích cậu nhìu nhìu',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh  à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Yêu em nhiều nhiêu ',
+    btnYes: 'Vẫn cứ là thích cậu <33',
+    btnNo: 'Không, deo:v',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại cứ thích tớ<3',
+    btnReply: 'Gửi cho tớ:3 <3',
+    reply: 'DÙ tớ có ấn gì thì tớ vẫn thích cậu:))',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Yêu lém, làm ngừi iu tớ nhé ',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/anhhmsd1105' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
