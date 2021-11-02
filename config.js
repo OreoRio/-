2a663888b1d9b41f9b6da34ac52cd2febf792e3f
OreoRio <86777052+OreoRio@users.noreply.github.com>
2021-11-02 22:31:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,19 @@
 const CONFIG = {
     introTitle: 'Bae à!!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao bạn cứ đi nhầm đường
-    Lạc vào tim mình lẻ loi :v
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'xàm tí cho vui thôi mà:))',
+    introDesc: `I'm takin'a chance
+    Cause I like you alot
+    So I'll give it a shot
+    Give it all that I got
+    So love me babe:(`,
+    btnIntro: 'love you forever:))',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Mình Yêu bạn :333#',
-    btnNo: 'Không, mình không thích bạn :(((',
-    question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình? :<',
+    btnYes: 'etou...tớ thích cậu:<',
+    btnNo: 'Không, mình không thích cậu :(((',
+    question:':< Trên thế giới hơn 7 tỷ ngừi shao cậu lại thích mềnh',
     btnReply: 'Gửi cho mình :D',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em biết mà 🥰. Yêu cậu nhiều nhiều :)))',
+    mess: 'Tớ đợi câu nì lâu lắm ùi🥰. Yêu cậu nhiều nhiều :)))',
     messDesc: 'Làm người yêu mình nhé? :D',
     btnAccept: 'Okiiiii lun <3',
     messLink: ' https://tranguynbakaa.com '.
